12fe38e35f380380246fe01a99aa550f1cfc7c44
tom <skirdetom@gmail.com>
2025-12-11 22:05:08 +0100

    Tailwind setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwind(),
     react(),
     legacy()
   ],
